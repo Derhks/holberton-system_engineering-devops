d06cbcdd2f066262ab8d4e86588aab95e771bba4
Derhks <derhks@gmail.com>
2020-04-05 01:56:46 +0000

    [Create] a puppet manifest to create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#  This manifest create a file in the directory /tmp
+
+file { 'create_file':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
